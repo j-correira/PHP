af6c58819613ff039fe312823fe4aace0452043e
unknown <jscorreira@email.neit.edu>
2019-04-02 00:40:55 -0400

    Update Credit Card with dynamic table

--- creditCard2.php
+++ creditCard2.php
@@ -4,20 +4,15 @@
     (-) calculate the number of months required to pay off the amount owed along with the total amount spent.
         Be sure to also include a month by month overview of how the amount owed goes down.
 
-
   */
 
-
-
     $errorMsg="";
-    $beginBalance=1000000;
+    $beginBalance=1000;
     $interest=15;
-    $payment=50;
+    $payment=100;
+    $month=0;
+    $totalPaid=0;
     $interestPaid2;
-    
-    //interestPaid = currentAmountOwed * interest / 100 / 12
-    
-
 
     if (isset($_POST['submit']))
     {
@@ -36,24 +31,9 @@
             $errorMsg .= "<li>Please make sure the Interest Rate is a number</li>";
         }
     }
-    
-    
-    //calculate()
-    //{
+
         $interestPaid = $beginBalance * $interest / 100 / 12;
-        //$interestPaid2 = $balance * $interest / 100;
-        //$runningTotal = $balance - $interestedPaid - 
-    //}
-/*       
-        while ($balance > 0)
-        {
-            $balance = $beginBalance;
-            
-            $balance = $balance - $payment - $interestPaid2;
-            
-            echo $balance . "<br>";
-        }
-*/              
+        
 ?>
 
 <!DOCTYPE html>
@@ -63,6 +43,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    
+    <style>
+        
+        body
+        {
+            margin:auto;
+            width: 522px;
+            padding:45px;
+            border-left: dotted 8px gray;
+            border-right: dotted 8px gray;
+        }
+        
+    </style>
+    
+    
     <title>J Correira</title>
 </head>
 <body>
@@ -76,43 +71,69 @@
     Interest Rate <input type="text" id="intRate" name="intRate" value="<?php echo $interest;?>">
     <br />
     Monthly Payment <input type="text" id="mnthPayment" name="mnthPayment" value="<?php echo $payment;?>">
-    <br />
+    <br>
+    <br>
 
     <input type="submit" id="submit" name="submit" onclick="">
-        <p>The interest paid = <?php echo number_format($interestPaid, 2);?></p>
-
-        <br>
-        <br>
+    <br>
+    <br>
+        
 <div class="alert alert-danger" role="alert">
   <?php echo $errorMsg; ?>
 </div>
-
-        
-        
+  
 </form>
+       <br>
 
+<table class="table table-striped" style="width:100%;">
+    <tr>
+        <th>Month</th>
+        <th>Interest Paid</th>
+        <th>Amount Owed</th>
+    </tr>
+    <tr>
+        <?php
+            $balance = $beginBalance;
+            $month = 0;
+            while ($balance > 0)
+            {
+                $month++;              
+                
+                $interestPaid = $balance * $interest / 100 / 12;
+                $balance = $balance - $payment + $interestPaid;
+                $totalPaid = $beginBalance + $interestPaid;
+                
+                echo "<tr>";
+                    //month column
+                    echo "<td>";
+                    echo $month;
+                    echo "</td>";
+                    
+                    //interest paid
+                    echo "<td>";
+                    echo "$" . number_format($interestPaid, 2);
+                    echo "</td>";
+                    
+                    //owed column
+                    echo "<td>";
+                    echo "$" . number_format($balance, 2);
+                    echo "</td>";
+                    
 
-    
-    
-    
-<table class="table table-striped" style="width:50%;">
-    <tr>
-        <th>Amount</th>
-        <th>Interest</th>
-    </tr>
-    <tr>
-        <td>1000</td>
-        <td>10.99</td>
+                echo "</tr>";
+            } 
+            
+            
+        ?>
         
-    </tr>
-    <tr>
-        <td>1000</td>
-        <td>10.99</td>
-        
-    </tr>
-    
 </table>
     
+    <br>
+    <br>
+    <h3>Total amount paid (Interest + Balance) : 
+        <br>
+        <?php echo "$" . number_format($totalPaid, 2); ?>
+    </h3>
     
 </body>
 </html>
